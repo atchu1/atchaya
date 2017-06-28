0d9ce075f3cfdd93e4f49dab7db856535ac0a4c3
atchu1 <atchayaraj18@gmail.com>
2017-06-28 14:46:03 +0530

    Update atchu.c

--- atchu.c
+++ atchu.c
@@ -1,1 +1,17 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+  int n;
+  clrscr();
+  printf("enter the number\n");
+  scanf("%d",&n);
+  if(a>0)
+    printf("the num is positive");
+  else if(a==0)
+    printf("the num is positive");
+  else
+    printf("the num is negative);
+           getch();
+           }
